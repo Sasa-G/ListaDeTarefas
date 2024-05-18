e336c38908faaa7fc0fa54bc0654931b27066761
Samanta Gomes De Morais Santos <samantagdms@gmail.com>
2024-05-18 17:41:57 -0300

    Enviando projeto para deploy

---
+++ ListadeTarefas/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'; 
+
+import ListaDeTarefas from './components/ListaDeTarefas';
+
+
+  
+
+function App() { 
+
+  return ( 
+
+    <div className="App"> 
+
+      <ListaDeTarefas /> 
+
+    </div> 
+
+  ); 
+
+} 
+
+  
+
+export default App; 
---
+++ ListadeTarefas/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+// import ListaDeTarefas from './components/ListaDeTarefas.jsx'
+// import 'ListaDeTarefas.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    
+  </React.StrictMode>,
+)
